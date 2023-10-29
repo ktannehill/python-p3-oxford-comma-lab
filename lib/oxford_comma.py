0c7b2c94905a137ee3835bf840b83c3846aa21b5
Kat Tannehill <katetannehill@gmail.com>
2023-10-28 22:37:07 -0600

    Complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
+import ipdb
+
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return " and ".join(items)
+    elif len(items) > 2:
+        last_item = items[-1]
+        items[-1] = "and " + last_item
+        return ", ".join(items)
+    else:
+        return "".join(items)
+
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
